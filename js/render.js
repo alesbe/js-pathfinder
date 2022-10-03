3c3bd28ed7b7bbc0b93608d14e2e030a4cfa0dfd
alesbe <aesparzabellver@gmail.com>
2022-10-03 08:32:53 +0000

    Change grid node type and navbar options

--- js/render.js
+++ js/render.js
@@ -1,30 +1,61 @@
+/* Grid selector */
 const grid = document.getElementById("main-grid");
 const gridWrapper = document.getElementById("grid-wrapper");
-const dropDownMenu = document.getElementById("select-algo-menu");
 
+/* Node type selectors */
+const wallNodeSelector = document.getElementById("wall-node-selector");
+const startingNodeSelector = document.getElementById("starting-node-selector");
+const targetNodeSelector = document.getElementById("target-node-selector");
+const clearNodeSelector = document.getElementById("clear-node-selector");
+
+/* Operation selector */
+const clearOperation = document.getElementById("clear-operation");
+const visualizeOperation = document.getElementById("visualize-operation");
+
+/* Enums */
+const NodeState = {
+  Wall: 0,
+  Starting: 1,
+  Target: 2,
+  Clear: 3
+}
+
+/* States */
+let nodeTypeState = 0;
+
+// Generate grid
 const nRows = 45;
 const nCols = 100;
 
-const DrawModes = {
-  Wall: 0,
-  StartingNode: 1,
-  TargetNode: 2,
-  ClearNode: 3
-}
-
-// Generate grid
 for (let row = 0; row < nRows; row++) {
   let row = document.createElement("div");
   row.className = "grid-row";
 
   for (let col = 0; col < nCols; col++) {
     let cell = document.createElement("div");
-    cell.className = "grid-cell";
+    cell.className = "grid-cell__clear";
 
     cell.onmousemove = function (e) {
       e.preventDefault();
       if (e.buttons == 1) {
-        cell.className = "grid-cell__wall";
+        console.log(nodeTypeState);
+        switch (nodeTypeState) {
+          case NodeState.Wall:
+            cell.className = "grid-cell__wall";
+            break;
+        
+          case NodeState.Starting:
+            cell.className = "grid-cell__starting";
+            break;
+
+          case NodeState.Target:
+            cell.className = "grid-cell__target";
+            break;
+
+          case NodeState.Clear:
+            cell.className = "grid-cell__clear";
+            break;
+        }
       }
     };
 
@@ -34,4 +65,24 @@ for (let row = 0; row < nRows; row++) {
   grid.appendChild(row);
 }
 
-// TODO: Setup navbar
+// Setup navbar
+wallNodeSelector.onclick = (e) => {
+  e.preventDefault();
+  nodeTypeState = NodeState.Wall;
+  console.log("Node type:", nodeTypeState);
+}
+startingNodeSelector.onclick = (e) => {
+  e.preventDefault();
+  nodeTypeState = NodeState.Starting;
+  console.log("Node type:", nodeTypeState);
+}
+targetNodeSelector.onclick = (e) => {
+  e.preventDefault();
+  nodeTypeState = NodeState.Target;
+  console.log("Node type:", nodeTypeState);
+}
+clearNodeSelector.onclick = (e) => {
+  e.preventDefault();
+  nodeTypeState = NodeState.Clear;
+  console.log("Node type:", nodeTypeState);
+}
